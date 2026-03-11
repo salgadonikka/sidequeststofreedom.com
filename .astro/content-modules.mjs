ff5b8e5711333675c36fb86835d16a11628c6f15
Nikka Paola Salgado <nikka.salgado@outlook.com>
2026-03-12 00:32:46 +0800

    Untrack node_modules and .astro from git

--- .astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/blog/mdx-demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmdx-demo.mdx&astroContentModuleFlag=true")],
-["src/content/blog/side-hustles-ranked-by-effort.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fside-hustles-ranked-by-effort.mdx&astroContentModuleFlag=true")]]);
-		
