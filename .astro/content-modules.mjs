98eafde1cc60d72f8acbe16ecf507133f18ffe90
Nikka Paola Salgado <nikka.salgado@outlook.com>
2026-03-11 23:26:30 +0800

    New Blog post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+export default new Map([
+["src/content/blog/mdx-demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmdx-demo.mdx&astroContentModuleFlag=true")],
+["src/content/blog/side-hustles-ranked-by-effort.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fside-hustles-ranked-by-effort.mdx&astroContentModuleFlag=true")]]);
+		
