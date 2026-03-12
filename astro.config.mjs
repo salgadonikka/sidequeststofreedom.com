a86edd459a08e6c4ab0186cb00bbb97d3d7729d6
Nikka Paola Salgado <nikka.salgado@outlook.com>
2026-03-12 22:41:48 +0800

    Update layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import pagefind from 'astro-pagefind';
 
 export default defineConfig({
   site: 'https://sidequeststofreedom.com',
-  integrations: [mdx()],
+  integrations: [mdx(), pagefind()],
 });
