42356720b5e78d6b6d0bb2e9e5997db21ffb141f
Nikka Paola Salgado <nikka.salgado@outlook.com>
2026-03-12 20:39:25 +0800

    Update blog and add AdSense

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
     tags: z.array(z.string()).optional(),
     draft: z.boolean().optional().default(false),
+    coverImage: image().optional(),
+    coverAlt: z.string().optional(),
   }),
 });
 
